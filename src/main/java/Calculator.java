715fec819e4c3b47f1e9b75c5a4aa30078d09b0d
Marta Moutinho <marta.barbosa-t22@upskill.pt>
2022-04-06 09:37:17 +0100

    método 6 adicionado

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,12 @@
 public class Calculator
 {
+ private double lastResultValue;
 
+ /**
+  Pessoal pode ser assim? No final de cada método onde ocorreu uma conta é só fazer this.lastResultValue = resulatado
+  */
+ public double getLastResultValue()
+ {
+  return lastResultValue;
+ }
 }
