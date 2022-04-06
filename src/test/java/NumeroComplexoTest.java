ea26f67eadf4713423206828cb674119c33030ec
Jose Aguiar <jose.aguiar-t22@upskill.pt>
2022-04-06 16:59:04 +0100

    test toString

--- src/test/java/NumeroComplexoTest.java
+++ src/test/java/NumeroComplexoTest.java
@@ -24,4 +24,16 @@ class NumeroComplexoTest
         NumeroComplexo actual = n1.produto (n2);
         assertEquals (expected, actual);
     }
+
+    @Test
+    void testPrints()
+    {
+        NumeroComplexo c1 = new NumeroComplexo (1,1);
+        NumeroComplexo c2 = new NumeroComplexo (1,0);
+        NumeroComplexo c3 = new NumeroComplexo (1,-1);
+
+        System.out.println (c1);
+        System.out.println (c2);
+        System.out.println (c3);
+    }
 }
