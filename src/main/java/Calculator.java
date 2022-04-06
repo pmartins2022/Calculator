d8de1a955b03b09452571c9c00f37d3845413bf1
MiguelLouzeiro <miguelant79@gmail.com>
2022-04-06 09:40:14 +0100

    Added metodo produto
    Added metodo subtracao

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,14 @@
 public class Calculator
 {
+    public double produto (double n1, double n2){
+        double prod;
+        prod=n1*n2;
+        return prod;
+    }
 
+    public double subtracao (double n1, double n2){
+        double sub;
+        sub=n1-n2;
+        return sub;
+    }
 }
