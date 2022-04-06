1bf371b6f3234b373ccbd949fa0b02009be16459
Jose Aguiar <jose.aguiar-t22@upskill.pt>
2022-04-06 16:25:40 +0100

    numero complexo Soma, Equals

---
+++ src/main/java/NumeroComplexo.java
@@ -0,0 +1,33 @@
+public class NumeroComplexo
+{
+    private double x,y;
+
+    public NumeroComplexo(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public NumeroComplexo somar(NumeroComplexo n2)
+    {
+        return new NumeroComplexo (this.x+n2.x, this.y+n2.y);
+    }
+
+    @Override
+    public String toString ()
+    {
+        return x+" + "+y+"i";
+    }
+
+    @Override
+    public boolean equals (Object o)
+    {
+        if (this == o) return true;
+        if (! (o instanceof NumeroComplexo)) return false;
+
+        NumeroComplexo that = (NumeroComplexo) o;
+
+        if (Double.compare (that.x, x) != 0) return false;
+        return Double.compare (that.y, y) == 0;
+    }
+}
