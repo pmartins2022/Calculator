6eb6f272c3817bb4e21682bae9b0466883ef2251
Pedro Martins <pedro.martins-t22@upskill.pt>
2022-04-06 09:59:13 +0100

    limpeza Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,25 +1,14 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-    /**
-     * Calcula o Produto de dois numeros
-     * @param n1 numero fornecido em primeiro lugar
-     * @param n2 numero fornecido em segundo lugar
-     * @return produto de n1 com n2
-     */
-=======
->>>>>>> bbcbcaf107b9bd417e299af6c0352887f820db8d
-public class Calculator {
-
+public class Calculator
+{
     private double lastResultValue;
 
     /**
      * Guarda resultado da operação na variável last resultValue
+     *
      * @param lastResultValue resultado da última operação
      */
-    public void memorizarResultadoOperacao(double lastResultValue) {
+    public void memorizarResultadoOperacao(double lastResultValue)
+    {
         this.lastResultValue = lastResultValue;
     }
 
@@ -27,64 +16,49 @@ public class Calculator {
     /**
      * Pessoal pode ser assim? No final de cada método onde ocorreu uma conta é só fazer this.lastResultValue = resulatado
      */
-    public double getLastResultValue() {
+    public double getLastResultValue()
+    {
         return lastResultValue;
     }
 
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> 8fd0c3b69099a73c91209cccdda5d88b7279da90
->>>>>>> bbcbcaf107b9bd417e299af6c0352887f820db8d
-    public double produto (double n1, double n2){
+    /**
+     * Calcula o Produto de dois numeros
+     *
+     * @param n1 numero fornecido em primeiro lugar
+     * @param n2 numero fornecido em segundo lugar
+     * @return produto de n1 com n2
+     */
+    public double produto(double n1, double n2)
+    {
         double prod;
-        prod=n1*n2;
+        prod = n1 * n2;
         return prod;
     }
 
     /**
      * Calcula a subtracao de dois numeros
+     *
      * @param n1 numero fornecid em primeiro lugar
      * @param n2 numero fornecido em segundo lugar
      * @return subtracao de n2 a n1
      */
-    public double subtracao (double n1, double n2){
+    public double subtracao(double n1, double n2)
+    {
         double sub;
-        sub=n1-n2;
+        sub = n1 - n2;
         return sub;
     }
-<<<<<<< HEAD
 
-    private double lastResultValue;
-
- /**
-  Pessoal pode ser assim? No final de cada método onde ocorreu uma conta é só fazer this.lastResultValue = resultado
-  */
-<<<<<<< HEAD
- public double getLastResultValue()
- {
-  return lastResultValue;
- }
-
-
- public double somar(double x, double y)
- {
-     double soma;
-     soma = x+y;
-  return soma;
- }
-
- public void apagarResultado()
- {
-  lastResultValue = Double.parseDouble (null);
- }
-=======
-    public double getLastResultValue()
+    public double somar(double x, double y)
     {
-        return lastResultValue;
+        double soma;
+        soma = x + y;
+        return soma;
     }
-=======
->>>>>>> 8fd0c3b69099a73c91209cccdda5d88b7279da90
->>>>>>> 19bbd43217b3da49005c4a93672ca5aa6ddc37f0
+
+    public void apagarResultado()
+    {
+        lastResultValue = Double.parseDouble(null);
+    }
 }
