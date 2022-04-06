6ea468a7e92c0361879b17bb39226d52780493de
Marta Moutinho <marta.barbosa-t22@upskill.pt>
2022-04-06 14:35:17 +0100

    testes 'getLastResultValue', 'potencia', 'binaryToDecimal'

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,45 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest
 {
+ Calculator calculator = new Calculator();
 
+ @Test
+ void getLastResultValue()
+ {
+  //Arrange
+
+  //Act
+  calculator.apagarResultado();
+  double result = calculator.getLastResultValue();
+  double expectedResult = 0;
+  //Assert
+  assertEquals(result, expectedResult);
+ }
+
+ @Test
+ void potencia()
+ {
+  //Arrange
+
+  //Act
+  double result = calculator.potencia(3, 3);
+  double expectedResult = 27;
+  //Assert
+  assertEquals(result, expectedResult);
+ }
+
+ @Test
+ void binaryToDecimal()
+ {
+  //Arrange
+
+  //Act
+  double result = calculator.binaryToDecimal(100);
+  double expectedResult = 4;
+  //Assert
+  assertEquals(result, expectedResult);
+ }
 }
