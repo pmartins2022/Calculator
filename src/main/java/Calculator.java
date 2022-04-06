685a6d2dc046dbcb3e291b2f079c622364f09880
Jose Aguiar <jose.aguiar-t22@upskill.pt>
2022-04-06 09:48:04 +0100

    adicionei os metodos 'somar' e 'apagarResultado'

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,10 +3,20 @@ public class Calculator
  private double lastResultValue;
 
  /**
-  Pessoal pode ser assim? No final de cada método onde ocorreu uma conta é só fazer this.lastResultValue = resulatado
+  Pessoal pode ser assim? No final de cada método onde ocorreu uma conta é só fazer this.lastResultValue = resultado
   */
  public double getLastResultValue()
  {
   return lastResultValue;
+ }
+
+ public double somar(int x, int y)
+ {
+  return x+y;
+ }
+
+ public void apagarResultado()
+ {
+  lastResultValue = Double.parseDouble (null);
  }
 }
