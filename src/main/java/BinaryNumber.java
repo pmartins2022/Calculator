8b18bee76f82e2d646a4e13c92087eba2848bb6c
Simao Mata <scfamata@gmail.com>
2022-04-06 16:30:13 +0100

    Soma binários

--- src/main/java/BinaryNumber.java
+++ src/main/java/BinaryNumber.java
@@ -1,10 +1,14 @@
-public class BinaryNumber
-{
- private int value;
+public class BinaryNumber {
+    private int value;
 
- public BinaryNumber(int value)
- {
-  this.value = value;
- }
+    public BinaryNumber(int value) {
+        this.value = value;
+    }
+
+
+    public int sumBinaries(int number) {
+        return this.value+number;
+    }
+
 }
 
