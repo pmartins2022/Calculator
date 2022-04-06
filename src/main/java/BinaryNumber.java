f02194287fbdad56b9656f6f94a8c6613929e151
Simao Mata <scfamata@gmail.com>
2022-04-06 16:46:54 +0100

    Binary Number Simao

--- src/main/java/BinaryNumber.java
+++ src/main/java/BinaryNumber.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class BinaryNumber {
     private int value;
 
@@ -14,10 +16,27 @@ public class BinaryNumber {
     }
 
 
-    public int sumBinaries(BinaryNumber number) {
-        return Integer.parseInt(Integer.toBinaryString(this.value))+Integer.parseInt(Integer.toBinaryString(number.getValue()));
+    public BinaryNumber sumBinaries(BinaryNumber number) {
+        int soma = ConversionUtils.binaryToDecimal(this.value)+ConversionUtils.binaryToDecimal(number.getValue());
+        return new BinaryNumber(Integer.parseInt(Integer.toBinaryString(soma)));
     }
 
+
+    public String toString(){
+        return String.format("%d", value);
+    }
+
+
+    public boolean equals (Object object){
+        if(this==object){
+            return false;
+        }
+        if(object==null || this.getClass()!=object.getClass()){
+            return false;
+        }
+        BinaryNumber object2 = (BinaryNumber) object;
+        return this.value==object2.getValue();
+    }
 
 }
 
