407ab3838436cf5b1369c828e41929a8595b3774
Marta Moutinho <marta.barbosa-t22@upskill.pt>
2022-04-06 16:34:06 +0100

    multiply método implementado

--- src/main/java/BinaryNumber.java
+++ src/main/java/BinaryNumber.java
@@ -1,10 +1,17 @@
 public class BinaryNumber
 {
- private int value;
+ private int value; // value = 1001      value = 1111
 
  public BinaryNumber(int value)
  {
   this.value = value;
  }
+
+ public int multiply(BinaryNumber binaryNumber)
+ {
+  return Integer.parseInt(String.valueOf(this.value)) * Integer.parseInt(String.valueOf(binaryNumber.value));
+ }
+
+
 }
 
