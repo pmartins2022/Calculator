081ca904a55131e2261a63ce16385bd723b54f96
MiguelLouzeiro <miguelant79@gmail.com>
2022-04-06 14:48:43 +0100

    testes subtracao e produto

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculatorTest
 {
     Calculator calc = new Calculator ();
+
     @Test
     void testSoma ()
     {
@@ -45,5 +46,45 @@ class CalculatorTest
         long result = c.fatorial(-5);
 
         assertEquals(-120, result);
+    }
+
+    @Test
+    void testProduto (){
+        double n1,n2,expected, actual;
+        n1 = 10;
+        n2 = 5;
+        expected = 15;
+        actual = calc.produto(n1,n2);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void testSubtracaoInteiros () {
+        double n1,n2,expected, actual;
+        n1 = 10;
+        n2 = 5;
+        expected=5;
+        actual = calc.subtracao(n1,n2);
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void testSubtracaoReais (){
+        double n1,n2,expected, actual;
+        n1 = 10.97f;
+        n2 = 0.12f;
+        expected = 10.85f;
+        actual = calc.subtracao(n1,n2);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSubtracaoResultadoNegativo (){
+        double n1,n2,expected, actual;
+        n1 = 0.5f;
+        n2 = 2.25f;
+        expected = 1.75f;
+        actual = calc.subtracao(n1,n2);
+        assertEquals(expected,actual);
     }
 }
