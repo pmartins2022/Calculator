6f5e28edc41b4d849d8fcdef6c81305c43b87ea6
Marta Moutinho <marta.barbosa-t22@upskill.pt>
2022-04-06 10:04:22 +0100

    javadoc atualizado para o método 6

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -12,9 +12,9 @@ public class Calculator
   this.lastResultValue = lastResultValue;
  }
 
-
  /**
-  * Pessoal pode ser assim? No final de cada método onde ocorreu uma conta é só fazer this.lastResultValue = resulatado
+  * Devolve valor do atributo lastResultValue
+  * @return valor do atributo lastResultValue
   */
  public double getLastResultValue()
  {
