8039a05dd0b44811e863471fd7d35831d156077f
Jose Aguiar <jose.aguiar-t22@upskill.pt>
2022-04-06 14:34:08 +0100

    "testes 1 e 14"

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,6 +4,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest
 {
+    Calculator calc = new Calculator ();
+    @Test
+    void testSoma ()
+    {
+        double y, x, expected, actual;
+        x = 10;
+        y = 5;
+        expected = 15;
+        actual = calc.somar (x, y);
+        assertEquals (expected, actual);
+    }
+
+    @Test
+    void testConverteDecimalParaHexa ()
+    {
+        int x;
+        String expected, actual;
+        x = 14;
+        expected = "e";
+        actual = calc.converteDecimalParaHexa (x);
+        assertEquals (expected, actual);
+    }
+
     @Test
     public void testCalculateFatorial1()
     {
