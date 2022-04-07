881b47f41db5fcedc05f61ef074636c3e1340e61
Simao Mata <scfamata@gmail.com>
2022-04-07 08:41:24 +0100

    Testes Simao

--- src/test/java/BinaryNumberTest.java
+++ src/test/java/BinaryNumberTest.java
@@ -2,41 +2,54 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BinaryNumberTest
-{
+class BinaryNumberTest {
 
- @Test
- void getBinaryValue()
- {
- }
+    BinaryNumber binNum = new BinaryNumber("101");
+    BinaryNumber binNum2 = new BinaryNumber("101");
 
- @Test
- void setBinaryValue()
- {
- }
+    @Test
+    void sum() {
+        //Arrange
+        BinaryNumber expectedResult = new BinaryNumber("1010");
 
- @Test
- void sum()
- {
- }
+        //Act
+        BinaryNumber result = binNum.sum(binNum2);
 
- @Test
- void multiply()
- {
- }
+        //Assert
+        assertEquals(expectedResult, result);
+    }
 
- @Test
- void subtraction()
- {
- }
+    @Test
+    void multiply() {
+    }
 
- @Test
- void testToString()
- {
- }
+    @Test
+    void subtraction() {
+    }
 
- @Test
- void testEquals()
- {
- }
+
+    @Test
+    void testEquals() {
+        //Arrange
+        boolean expectedResult = true;
+
+        //Act
+        boolean result = binNum.equals(binNum);
+
+        //Assert
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void testEquals2() {
+        //Arrange
+        Object object = new Object();
+        boolean expectedResult = false;
+
+        //Act
+        boolean result = binNum.equals(object);
+
+        //Assert
+        assertEquals(expectedResult, result);
+    }
 }
