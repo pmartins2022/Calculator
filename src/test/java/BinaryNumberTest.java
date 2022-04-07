1c1085b39fb93b038b1c689619a4c0545aa8bf95
Marta Moutinho <marta.barbosa-t22@upskill.pt>
2022-04-07 08:23:43 +0100

    binaryNumberTest criado

---
+++ src/test/java/BinaryNumberTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryNumberTest
+{
+
+ @Test
+ void getBinaryValue()
+ {
+ }
+
+ @Test
+ void setBinaryValue()
+ {
+ }
+
+ @Test
+ void sum()
+ {
+ }
+
+ @Test
+ void multiply()
+ {
+ }
+
+ @Test
+ void subtraction()
+ {
+ }
+
+ @Test
+ void testToString()
+ {
+ }
+
+ @Test
+ void testEquals()
+ {
+ }
+}
