e2500db4e6009c9bec1d751446c07bb43435d87b
Simao Mata <scfamata@gmail.com>
2022-04-07 08:18:47 +0100

    Binary Simao

--- src/main/java/BinaryNumber.java
+++ src/main/java/BinaryNumber.java
@@ -2,51 +2,53 @@ import java.util.Objects;
 
 public class BinaryNumber {
 
-        private int binaryValue;
+    private String binaryValue;
 
-        public BinaryNumber(int value) {
-            this.binaryValue = value;
-        }
-
-
-        public int getValue() {
-            return binaryValue;
-        }
-
-        public void setValue(int value) {
-            this.binaryValue = value;
-        }
-
-        public BinaryNumber sumBinaries(BinaryNumber number) {
-            int soma = ConversionUtils.binaryToDecimal(this.binaryValue) + ConversionUtils.binaryToDecimal(number.getValue());
-            return new BinaryNumber(Integer.parseInt(Integer.toBinaryString(soma)));
-        }
-
-        public int multiply(BinaryNumber binaryNumber) {
-            int x = Integer.parseInt(String.valueOf(this.binaryValue), 2) * Integer.parseInt(String.valueOf(this.binaryValue), 2);
-            return Integer.parseInt(Integer.toBinaryString(x));
-        }
-
-        public int subtraction(BinaryNumber binaryNumber) {
-            int x = Integer.parseInt(Integer.toBinaryString(this.binaryValue)) - Integer.parseInt(Integer.toBinaryString(binaryNumber.getValue()));
-            return Integer.parseInt(Integer.toBinaryString(x));
-        }
-
-        public String toString() {
-            return String.format("%d", binaryValue);
-        }
-
-
-        public boolean equals(Object object) {
-            if (this == object) {
-                return false;
-            }
-            if (object == null || this.getClass() != object.getClass()) {
-                return false;
-            }
-            BinaryNumber object2 = (BinaryNumber) object;
-            return this.binaryValue == object2.getValue();
-        }
-
+    public BinaryNumber(String value) {
+        this.binaryValue = value;
     }
+
+
+    public String getBinaryValue() {
+        return binaryValue;
+    }
+
+    public void setBinaryValue(String binaryValue) {
+        this.binaryValue = binaryValue;
+    }
+
+
+    public BinaryNumber sum(BinaryNumber number) {
+        int sum= ConversionUtils.binaryToDecimal(Integer.parseInt(this.binaryValue)) +
+                ConversionUtils.binaryToDecimal(Integer.parseInt(number.getBinaryValue()));
+        return new BinaryNumber(ConversionUtils.decimalToBinary(sum));
+    }
+
+    public int multiply(BinaryNumber binaryNumber) {
+        int x = Integer.parseInt(String.valueOf(this.binaryValue), 2) * Integer.parseInt(String.valueOf(this.binaryValue), 2);
+        return Integer.parseInt(Integer.toBinaryString(x));
+    }
+
+    public int subtraction(BinaryNumber binaryNumber) {
+        int x = Integer.parseInt(Integer.toBinaryString(this.binaryValue)) - Integer.parseInt(Integer.toBinaryString(binaryNumber.getValue()));
+        return Integer.parseInt(Integer.toBinaryString(x));
+    }
+
+    public String toString() {
+        return binaryValue;
+    }
+
+
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || this.getClass() != object.getClass()) {
+            return false;
+        }
+        BinaryNumber object2 = (BinaryNumber) object;
+        return this.binaryValue.equalsIgnoreCase(object2.getBinaryValue());
+    }
+
+}
 
